65e628cb26ea924e6dd76071dba625fb6b165ea3
Roei Barak <60481692+Roei-Barak@users.noreply.github.com>
2025-05-22 00:56:23 +0300

    Update personalInfo.ts

--- src/data/personalInfo.ts
+++ src/data/personalInfo.ts
@@ -1,9 +1,10 @@
 export const personalInfo = {
   name: "Roei Barak",
   title: "Computer Science Student & Cybersecurity Enthusiast",
-  email: "roeibarak123@tormail.onion", // Using .onion email service
+  email: "RoeiBarak123@gmail.com", 
   phone: "050-5854505",
-  github: "http://githubxxx.onion/Roei-Barak", // Example .onion address
-  linkedin: "http://linkedinxxx.onion/in/RoeiBarak", // Example .onion address
+  Website: "RoeiBarak.com",
+  github: "https://github.com/Roei-Barak/", // Example .onion address
+  linkedin: "www.linkedin.com/in/roeibarak", // Example .onion address
   about: "Last year Computer Science student with a focus on cybersecurity and ethical hacking. Graduator of MCSA course with 4 years of experience in IT and system administration. Highly motivated, autodidact, and quick learner with excellent interpersonal skills."
 };
